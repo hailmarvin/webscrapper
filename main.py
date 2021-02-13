80a62790a7b53c983e25f7d13d7ffeb5c897a001
DevMarvin <mmwanzi76@gmail.com>
2021-02-13 19:36:40 +0300

    iterate through class name

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ url = ""
 page = requests.get(url)
 parsed = BeautifulSoup(page.content, "html.parser")
 
-#Specify element by id
+# Specify element by id
 results = parsed.find(id="")
 
 print(results.prettify())
+
+# Iterate through elements under same classname
+elems = results.find_all('section', class_='')
+
+for elem in elems:
+    print(elem, end='/n')
